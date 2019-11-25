a4502254df03a929d367c0d40052df6d57fb1903
alijalal-sys <ajmabilal@gmail.com>
2019-11-25 09:14:46 +0100

    Toy Problem

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,28 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  var arrOfChars = string.split(''); // Create an array of the Chars
+  var isNotMatched = false;
+  // What i am trying to do is take each char in the array and check 
+  // and iterate over the array again 
+  // if matched break of the second loop and keep iterating 
+  // if not matched set the value of var isNotMatched to true
+  // at the end if NotMatched Is True return the elm2
+  for (var i = 0; i < arrOfChars.length; i++) {
+  	var elm = arrOfChars[i];
+  	for(var j = 0; j < arrOfChars.length; j++){
+  		var elm2 = arrOfChars[j];
+  		if(elm === elm2){
+  			break;
+  		}
+  		if(elm !== elm2){
+  			isNotMatched = true;
+  			continue;
+  			console.log(elm, elm2)
+  		}
+  	}
+  	if( isNotMatched ){
+  		return elm2;
+  	}
+  }
 };
