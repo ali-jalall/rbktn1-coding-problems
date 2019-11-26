75069436275e599dba288c62ed84a360e0da2371
alijalal-sys <ajmabilal@gmail.com>
2019-11-26 09:32:53 +0100

    BlackJack alomst Done

--- blackjackScorer/blackjack.js
+++ blackjackScorer/blackjack.js
@@ -13,3 +13,41 @@ Examples
 ["A", "10", "A"]                ==>  12
 ["5", "3", "7"]                 ==>  15
 ["5", "4", "3", "2", "A", "K"]  ==>  25 */
+
+function blackJack(array){
+	var result = 0;
+	// First we i need to add a RegEx for the numbers to check inside the loop
+	var regNumbers = /[0-9]/g;
+	for(var i = 0; i < array.length; i++){
+		// So if the index = 'A' And the array includes Number
+		if(array[i] === 'A' && regNumbers.test(array)){
+			// Here we count the 'A' as 1
+			result += 1;
+			continue;
+		}
+		// Here if the index = 'A' and the array doesn't contains numbers
+		if(array[i] === 'A' && !regNumbers.test(array)){
+			// We count the 'A' as 11
+			result += 11;
+		}
+
+		// And of course the 'J Q K' Count as 10
+		if(array[i] === 'J' || array[i] === 'Q' || array[i] === 'K'){
+			result += 10;
+			continue;
+		}
+
+		// And here we check if this index is number by using RegEx
+		if(regNumbers.test(array[i])){
+			console.log(parseInt(array[i]))
+			// We Conver it to Number because the whole array is string
+			result += parseInt(array[i]);
+			continue;
+		}
+	}
+	// And return
+	return result;
+
+	// Logically it should work and it works on some of the examples 
+	// but i don't know what is missing
+}
