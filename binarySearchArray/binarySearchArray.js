a0dd6654f870112944af1cfb0381de23f04f7847
alijalal-sys <ajmabilal@gmail.com>
2019-12-03 09:09:40 +0100

    Binary search Done!

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,22 @@
  */
 
 var binarySearch = function (array, target) {
+	// Array is Sorted
+	// So we Save the Middle element in the array each time we iterate
+	var medium = Math.round(array.length / 2);
+	// And itertate over the array
+	for (var i = 0; i < array.length; i++) {
+		// Our stop condition is when you found the value
+		if (array[i] === target) {
+			return i;
+		}
+		// if NOT so return re-invoke the function with slicing the array from the middle index
+		if (target > array[medium]) {
+			binarySearch (array.slice(medium), target)
+		}
+	}
+
+	// Nothing found return null
+	return null
 };
 
