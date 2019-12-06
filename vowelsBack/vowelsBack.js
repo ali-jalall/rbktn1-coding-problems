fa05264918f0116f1c9a2156ffa5b0ffcfcc5603
alijalal-sys <ajmabilal@gmail.com>
2019-12-06 09:46:39 +0100

    vowels back

--- vowelsBack/vowelsBack.js
+++ vowelsBack/vowelsBack.js
@@ -11,3 +11,45 @@
 // If a moved letter becomes 'c', 'o', 'd' or 'e', revert it back to it's original value.
 
 // Provided string will always be lower case, won't be empty and will have no special characters.
+
+function vowelsBack(str) {
+	let chars = 'abcdefghijklmnopqrstuvwxyz'.split(''); // Array Of the Alphabet
+	let regVowels = /[oiuea]/;
+	let arrOfStr = str.split(''); // chars of our string
+	for (var i = 0; i < arrOfStr.length; i++) {
+
+		if ( regVowels.test(arrOfStr[i]) ) {
+			if (arrOfStr[i] === 'o') {
+				arrOfStr[i] = chars[chars.indexOf(arrOfStr[i]) - 1];
+				continue;
+			}
+			if (arrOfStr[i] === 'e') {
+				arrOfStr[i] = chars[chars.indexOf(arrOfStr[i]) - 4];
+				continue;
+			}
+			if ( chars[chars.indexOf(arrOfStr[i]) - 4] === 'a' ) {
+				arrOfStr[i] = chars[chars.length - 1] - 5;
+				continue;
+			}
+			arrOfStr[i] = chars[chars.indexOf(arrOfStr[i]) - 5];
+			continue;
+		}
+
+		if (arrOfStr[i] === 'c') {
+			arrOfStr[i] = chars[chars.indexOf(arrOfStr[i]) - 1];
+			continue;
+		}
+
+		if (arrOfStr[i] === 'd') {
+			arrOfStr[i] = chars[chars.indexOf(arrOfStr[i]) - 4];
+			continue;
+		}
+
+		if (chars[chars.indexOf(arrOfStr[i]) + 8] === 'z') {
+			arrOfStr[i] = chars[chars[0] + 9];
+			continue;
+		}
+		arrOfStr[i] = chars[chars.indexOf(arrOfStr[i]) + 9];
+	}
+	return arrOfStr.join('')
+}
