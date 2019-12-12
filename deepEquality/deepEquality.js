6759c4e3e85d1056e341d24b6f5ce75bbce171fc
alijalal-sys <ajmabilal@gmail.com>
2019-12-12 09:37:24 +0100

    deep equality

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -11,5 +11,83 @@
   * don't worry about handling cyclical object structures.
   *
   */
+
+	// console.log(Object.keys(apple) === Object.keys(orange))
+	// var keys = Object.keys(orange);
+	// var values = Object.values(orange);
+	// console.log(values)
+
+
+	// debugger;
+	// var appleEntries = Object.entries(apple)
+	// var orangeEntries = Object.entries(orange)
+	// var isEqual = false;
+	// for (var i = 0; i < appleEntries.length; i++) {
+	// 	for (var i = 0; i < orangeEntries.length; i++) {
+	// 		if( appleEntries[i] === orangeEntries[i]) {
+	// 			isEqual = true;
+	// 			continue;
+	// 		} else {
+	// 			return false
+	// 		}
+	// 	}
+	// }
+	// return isEqual
+
+	// var isEqual = false;
+	// for (let [key, value] of Object.entries(apple)) {
+	//   for (let [key2, value2] of Object.entries(orange)) {
+	//    	if (key === key2 && value === value2) {
+	//    		isEqual = true;
+	//    		break;
+	//    	} else {
+	//    		isEqual = false
+	//    	}
+	// 	}
+	// }
+	// return isEqual;
+	
+
+	// var isEqual = false;
+	// var keys1 = Object.keys(apple);
+	// var values1 = Object.values(apple);
+	// var keys2 = Object.keys(orange);
+	// var values2 = Object.values(orange);
+	// for (var i = 0; i < keys1.length; i++) {
+	// 	for (var j = 0; j < keys2.length; j++) {
+	// 		if (keys[i] === keys2[j]) {
+	// 			isEqual = true;
+	// 		} else {
+	// 			return false;
+	// 		}
+	// 	}
+	// }
+
+	// for (var i = 0; i < values1.length; i++) {
+	// 	for (var j = 0; j < values1.length; j++) {
+	// 		if (values1[i] === values2[j]) {
+	// 			isEqual = true;
+	// 		} else {
+	// 			return false;
+	// 		}
+	// 	}
+	// }
+
+
+	// return isEqual;
+	// // for (var key in apple) {}
+
 var deepEquals = function(apple, orange) {
+	var isEqual = false;
+	for (let [key, value] of Object.entries(apple)) {
+	  for (let [key2, value2] of Object.entries(orange)) {
+	   	if (key === key2 && value === value2) {
+	   		isEqual = true;
+	   		break;
+	   	} else {
+	   		isEqual = false
+	   	}
+		}
+	}
+	return isEqual;
 };
