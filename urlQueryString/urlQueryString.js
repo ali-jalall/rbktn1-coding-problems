cf0756846541cefb7e93d205fdb04b543f36bfb2
alijalal-sys <ajmabilal@gmail.com>
2019-12-13 09:42:54 +0100

    url

--- urlQueryString/urlQueryString.js
+++ urlQueryString/urlQueryString.js
@@ -9,4 +9,41 @@ parseQueryString("http://example.com?msg=hello%20world") // [ [ "msg", "hello wo
 parseQueryString("http://example.com") // undefined
 */
 
-function parseQueryString(url) {}
+function parseQueryString(url) {
+	var regEx = /[0-9]/g;
+	// final result
+	var newArr = [];
+	// new String to save the text after ?
+	var newStr = '';
+	// clean string
+	var cleanStr = ''
+	// iterate over string and amd take the text after ? mark !
+	for (var i = 0; i < url.length; i++) {
+		if (url[i] === '?') {
+			newStr = url.slice(i + 1)
+		}
+	}
+
+	console.log(newStr)
+	// iterate over new String 
+	for (var i = 0; i < newStr.length; i++) {
+		// seperate are '=' mark
+		if (newStr[i] === '=') {
+			// text after ? mark save it inside seperate arrays
+			var endElement = newStr.slice(i + 1, newStr.indexOf('&'));
+			var endElement2 = newStr.slice(i + 1, newStr[newStr.length - 1]);
+			var startElement = newStr.slice(newStr[0], newStr.indexOf('='))
+			// console.log(startElement)
+			if ( endElement ) {
+				newArr.push([startElement, endElement])
+			} else 
+			newArr.push([startElement, endElement2])
+		}
+	}
+
+	if (newArr.length === 0) {
+		return undefined;
+	}
+
+	return newArr;
+}
