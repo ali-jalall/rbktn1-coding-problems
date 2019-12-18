6a5ac4484e4557aa4e1fbe2ab0d22167e6c5d7f2
alijalal-sys <ajmabilal@gmail.com>
2019-12-18 09:43:50 +0100

    Am suck at trees!

--- treeBFSelect/treeBFSelect.js
+++ treeBFSelect/treeBFSelect.js
@@ -1,54 +1,40 @@
-/**
-  *
-  * Implement a `BFSelect` method on this Tree class.
-  *
-  * BFSelect accepts a filter function, calls that function on each of the nodes
-  * in Breadth First order, and returns a flat array of node values of the tree
-  * for which the filter returns true.
-  *
-  * Example:
-  *   var root1 = new Tree(1);
-  *   var branch2 = root1.addChild(2);
-  *   var branch3 = root1.addChild(3);
-  *   var leaf4 = branch2.addChild(4);
-  *   var leaf5 = branch2.addChild(5);
-  *   var leaf6 = branch3.addChild(6);
-  *   var leaf7 = branch3.addChild(7);
-  *   root1.BFSelect(function (value, depth) {
-  *     return value % 2;
-  *   })
-  *   // [1, 3, 5, 7]
-  *
-  *   root1.BFSelect(function (value, depth) {
-  *     return depth === 1;
-  *   })
-  *   // [2, 3]
-  *
-  */
-
-/*
- * Basic tree that stores a value.
- */
-
 var Tree = function(value) {
   this.value = value;
   this.children = [];
 };
 
+var getChildren = function (arr, filter) {
+    var myArr = [];
+    for (var i = 0; i < arr.length; i++) {
+//         console.log(arr.children[i])
+        if ( filter(arr[i].value) ) {
+            console.log(arr[i].value)
+            myArr.push(arr[i].value);
+        }
+    }
+//     return myArr;
+}
 
-
+// // // // // // // 
 Tree.prototype.BFSelect = function(filter) {
-  // return an array of values for which the function filter(value, depth) returns true
+    debugger
+
+  var newArr = [];
+  for (var i = 0; i < this.children.length; i++) {
+//     console.log(this.children[i]);
+
+    if ( filter(this.children[i].value) ) {
+      newArr.push(this.children[i].value);
+    }
+
+    if ( this.children[i].children ) {
+      getChildren(this.children[i])
+    }
+  }
+//   return newArr;
 };
+// // // // // // // 
 
-/**
- * You shouldn't need to change anything below here, but feel free to look.
-  */
-
-/**
-  * add an immediate child
-  * (wrap values in Tree nodes if they're not already)
-  */
 Tree.prototype.addChild = function(child) {
   if (!child || !(child instanceof Tree)) {
     child = new Tree(child);
@@ -59,22 +45,16 @@ Tree.prototype.addChild = function(child) {
   } else {
     throw new Error('That child is already a child of this tree');
   }
-  // return the new child node for convenience
   return child;
 };
 
-/**
-  * check to see if the provided tree is already a child of this
-  * tree __or any of its sub trees__
-  */
+
 Tree.prototype.isDescendant = function(child) {
   if (this.children.indexOf(child) !== -1) {
-    // `child` is an immediate child of this tree
     return true;
   } else {
     for (var i = 0; i < this.children.length; i++) {
       if (this.children[i].isDescendant(child)) {
-        // `child` is descendant of this tree
         return true;
       }
     }
@@ -82,15 +62,24 @@ Tree.prototype.isDescendant = function(child) {
   }
 };
 
-/**
-  * remove an immediate child
-  */
+
 Tree.prototype.removeChild = function(child) {
   var index = this.children.indexOf(child);
   if (index !== -1) {
-    // remove the child
     this.children.splice(index, 1);
   } else {
     throw new Error('That node is not an immediate child of this tree');
   }
 };
+
+// Testing
+var root1 = new Tree(1);
+var branch2 = root1.addChild(2);
+var branch3 = root1.addChild(3);
+var leaf4 = branch2.addChild(4);
+var leaf5 = branch2.addChild(5);
+var leaf6 = branch3.addChild(6);
+var leaf7 = branch3.addChild(7)
+root1.BFSelect(function (value, depth) {
+  return value === 2;
+})
