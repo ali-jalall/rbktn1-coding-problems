8d01f076f613953b0ed7596ea88855eca86528d6
alijalal-sys <ajmabilal@gmail.com>
2019-12-20 09:06:46 +0100

    Done

--- balancedParenthese/balancedParentheses.js
+++ balancedParenthese/balancedParentheses.js
@@ -8,4 +8,23 @@ isBalanced("(50)(")	// false
 isBalanced("") //	true
 */
 
-var isBalanced = function(str) {};
+// My solution is to count the open parentheses & the close parentheses
+// return the condition if they are even or no
+var isBalanced = function(str) {
+	var openParentheses = '(';
+	var closeParentheses = ')';
+	var counterOpen = 0;
+	var counterClose = 0;
+	if (str === '') {
+		return true;
+	}
+	for (var i = 0; i < str.length; i++) {
+		if (str[i] === openParentheses) {
+			counterOpen++;
+		}
+		if (str[i] === closeParentheses) {
+			counterClose++;
+		}
+	}
+	return counterOpen === counterClose;
+};
