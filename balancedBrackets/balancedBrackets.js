dc7a375991b9cb0a1b5382e44cf87e15a4c7c514
alijalal-sys <ajmabilal@gmail.com>
2019-12-24 09:19:01 +0100

    Balanced brackets ):

--- balancedBrackets/balancedBrackets.js
+++ balancedBrackets/balancedBrackets.js
@@ -9,4 +9,64 @@ isBalanced("(50)(") // false
 isBalanced("[{]}") // false
 */
 
-var isBalanced = function(str) {};
+
+var isBalanced = function(str) {
+  // debugger;
+  var parenthesis = '(';
+  var parenthesisClose = ')';
+  var brackets = '[';
+  var bracketsClose = ']';
+  var curly = '{';
+  var curlyClose = '}';
+
+  var isBalanced = false;
+
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === parenthesis) {
+      for (var j = i; j < str.length; j++) {
+        if (str[j] === bracketsClose || str[j] === curlyClose) {
+          return false;
+        }
+      }
+      if ( str.includes(parenthesisClose, i) ) {
+        isBalanced = true;
+      } else {
+        return false
+      }
+    }
+  }
+
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === brackets) {
+      for (var j = i; j < str.length; j++) {
+        if (str[j] === parenthesisClose || str[j] === curlyClose) {
+          return false;
+        }
+      }
+      if ( str.includes(bracketsClose, i) ) {
+        isBalanced = true;
+      } else {
+        return false
+      }
+    }
+  }
+
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === curly) {
+      for (var j = i; j < str.length; j++) {
+        if (str[j] === parenthesisClose || str[j] === bracketsClose) {
+          return false;
+        }
+      }
+      if ( str.includes(curlyClose, i) ) {
+        isBalanced = true;
+      } else {
+        return false
+      }
+    }
+  }
+
+  return isBalanced;
+};
+
+isBalanced("[{]}")
