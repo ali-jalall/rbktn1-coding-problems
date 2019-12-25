b5bbdcd873e6f88efc1aa0c270630448cea93514
alijalal-sys <ajmabilal@gmail.com>
2019-12-25 09:41:38 +0100

    :)(

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -16,9 +16,35 @@
 * Example:
 * rockPaperScissors(5); // => ['RRRRR', 'RRRRP', 'RRRRS', etc...]
 *
+var str = '';
+  var array = [];
+  var rockPaperScissors = ['R', 'P', 'S'];
+  // AS AN EXAMPLE
+  // we need to set the string to be 'RRR' as start point
+  var str = 'RRR';
+  // then we need to start from the last index and change it each time to be rockPaperScissors
+  for (var i = str.length - 1; i >= 0; i--) {
+    str[i] += rockPaperScissors[i];
+  }
+  // if the rockPaperScissors array ends!
+    // descrease the length by 1 and start replacing again
+  return str
+  // as always i know the idea but i don't know how to do it
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function () {
+  var str = '';
+  var array = [];
+  var rockPaperScissors = ['R', 'P', 'S'];
+  // AS AN EXAMPLE
+  // we need to set the string to be 'RRR' as start point
+  var str = 'RRR';
+  // then we need to start from the last index and change it each time to be rockPaperScissors
+  for (var i = str.length - 1; i >= 0; i--) {
+    str[i] += rockPaperScissors[i];
+  }
+  // if the rockPaperScissors array ends!
+    // descrease the length by 1 and start replacing again
+  return str
+  // as always i know the idea but i don't know how to do it
 };
