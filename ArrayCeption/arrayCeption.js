b4c000ad2f5c5d7391efdb8e211aa2a9d4ff9092
alijalal-sys <ajmabilal@gmail.com>
2020-01-14 09:55:52 +0100

    array ception Almost done :(

--- ArrayCeption/arrayCeption.js
+++ ArrayCeption/arrayCeption.js
@@ -15,3 +15,26 @@
 //  [ ] ==>	0
 //  array:
 //  [ [ [ ] ] ] ==>	0
+
+
+//  Almost Done :(
+  
+let level = 1;
+let newObj = {};
+const arrayCeption = (array) => {
+  for (var i = 0; i < array.length; i++) {
+    if ( !Array.isArray(array[i]) ) {
+      newObj[array[i]] = level;
+      continue;
+    } else {
+      level++;
+      arrayCeption(array[i]);
+    }
+  }
+  console.log(newObj)
+  return level
+}
+
+
+// level++
+// return level
