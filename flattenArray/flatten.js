454b4a5ee494899659e69335706f1aa7ce098f82
alijalal-sys <ajmabilal@gmail.com>
2020-02-08 09:42:52 +0100

    flatten array refactored

--- flattenArray/flatten.js
+++ flattenArray/flatten.js
@@ -6,11 +6,15 @@ Hint: Use Array.isArray(elem) to see if something is an array.
 Example: 
 flatten([[1, 1], [4], 3, [3], 4, [6, 7]]) // [1, 1, 4, 3, 3, 4, 6, 7]
 */
-let i = 0;
-function flatten(arr, iterator) {
-  if (iterator === 5) {
-    return arr.flat(Infinity);
-  }
-
-  return flatten(arr, i++)
+function flatten(arr, result = []) {
+  arr.forEach(element => {
+    if (Array.isArray(element)) {
+      flatten(element, result)
+    } else {
+      result.push(element)
+    }
+  });
+  return result
 }
+
+console.log(flatten([[1, 1], [4], 3, [3], 4, [6, 7]]))
