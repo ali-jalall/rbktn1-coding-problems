e7473fdc1670861d49cf9193eed77f9e39c5a907
alijalal-sys <ajmabilal@gmail.com>
2020-03-04 09:15:24 +0100

    sum of primes solved

--- sumOfPrimes/sumOfPrime.js
+++ sumOfPrimes/sumOfPrime.js
@@ -1,8 +1,26 @@
 /*
 Sum of Primes
-Given a positive number, return the sum of all positive primes that are less than or equal to that number.
+Given a positive number, return the sum of all positive primes 
+that are less than or equal to that number.
 */
 
-function sumOfPrimes(n) {
-    // your code here...
+const isPrime = num => {
+  for ( let i = 2; i < num; i++ ) {
+    if ( num % i === 0 ) {
+      return false;
+    }
+  }
+  return true;
 }
+
+
+
+const sumOfPrimes = num => {
+  let sum = 2;
+  for ( let i = 3; i < num; i+=2 ) {
+    isPrime(i) ?  sum += i : null
+  }
+  return sum;
+}
+
+console.log(sumOfPrimes(13))
